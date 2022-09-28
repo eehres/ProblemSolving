f9d2dc6d236f6d4b33b42f0275e1d4ee0dcfd0ef
eehres <89247924+eehres@users.noreply.github.com>
2022-09-28 17:55:38 +0900

    [Silver IV] Title: 듣보잡, Time: 336 ms, Memory: 26720 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        HashSet<String> set = new HashSet<>(); 
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
+
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            String s = br.readLine();
+            if(set.contains(s)){
+                result.add(s);
+            }
+        }
+
+        Collections.sort(result);
+        System.out.println(result.size());
+        for (String s : result) {
+            System.out.println(s);
+        }
+    }
+}
