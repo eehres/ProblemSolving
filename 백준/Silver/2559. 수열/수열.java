f6a4f4c1f37bc3aac01945cb718caa5cba68600d
eehres <89247924+eehres@users.noreply.github.com>
2022-10-04 20:02:05 +0900

    [Silver III] Title: 수열, Time: 1704 ms, Memory: 104308 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+       Scanner sc = new Scanner(System.in);
+       int N = sc.nextInt();
+       int K = sc.nextInt();
+
+        int arr[] = new int[N];
+        for(int i = 0 ; i < N ; i++){
+            arr[i] = sc.nextInt();
+        }
+        int max = Integer.MIN_VALUE;
+        
+        for(int i = 0; i <= N-K ; i++){
+            int sum = 0;
+            for(int j = i; j <i+K ; j++){
+                sum += arr[j];
+            }
+            max = Math.max(max,sum);
+        }
+
+        System.out.println(max);
+    }
+}
