db75a7ba89a6c97db5f5a4cc67def1965e4cd49c
eehres <89247924+eehres@users.noreply.github.com>
2022-10-08 13:16:17 +0900

    Update 벌집.py

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,9 +1,15 @@
-n = int(input())
-i = 1
-result = 1
-
-while(n>i):
-    i+=6*result
-    result+=1
-print(result)
-    
+import java.util.Scanner;
+class Main{
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+        int result = 1;
+        
+        while(n>i){
+            i+=6*result;
+            result++;
+        }
+		System.out.println(result);
+	}
+}
