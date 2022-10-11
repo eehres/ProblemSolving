62560e3a9f2115a982a0d9b90bd4d06c1aec8a85
eehres <89247924+eehres@users.noreply.github.com>
2022-10-11 20:16:37 +0900

    Delete 단어 공부.py

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++
@@ -1,9 +0,0 @@
-s = input().upper()
-count_list = []
-s_list = list(set(s))
-for i in s_list:
-    count_list.append(s.count(i))
-if count_list.count(max(count_list))>1:
-    print("?")
-else:
-    print(s_list[count_list.index(max(count_list))])
