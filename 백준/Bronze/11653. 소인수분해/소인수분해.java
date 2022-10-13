ef7eb0eb95f9f3bb8705fde5a7d1ac04ec45e5e1
eehres <89247924+eehres@users.noreply.github.com>
2022-10-13 23:44:00 +0900

    [Bronze I] Title: 소인수분해, Time: 244 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Main{
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int i = 2;
+        while(N!=1){
+            if(N%i==0){
+                N/=i;
+                System.out.println(i);
+            }else{
+                i++;
+            }
+        }
+		
+	}
+}
