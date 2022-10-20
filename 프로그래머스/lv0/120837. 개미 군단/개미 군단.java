0000ff5fdfc35fc158a7abea656e7e304d9dfb4a
eehres <89247924+eehres@users.noreply.github.com>
2022-10-20 20:17:38 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        int [] arr = {5,3,1};
+        for(int i = 0 ; i< 3 ; i++){
+            answer += hp/arr[i];
+            hp %= arr[i];
+        }
+        return answer;
+    }
+}
