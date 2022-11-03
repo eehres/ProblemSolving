b05b7685782e17255f8a44763f59e9a59cf85e0e
eehres <89247924+eehres@users.noreply.github.com>
2022-11-03 20:41:37 +0900

    Update and rename 손익분기점.py to 손익분기점.java

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main{
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        
+        if(B>=C){
+            System.out.println("-1");
+        }else{
+            System.out.println(A/(C-B)+1);
+        }
+	}
+}
