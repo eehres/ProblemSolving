5cb1ff02a54e5aa175e795ef009e7bed24224a39
eehres <89247924+eehres@users.noreply.github.com>
2022-12-07 23:14:04 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.11 ms, Memory: 63.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int solution(int n) {    
+        int count = 0;
+        for(int i = 1; i <= n ; i++){
+            count++;
+            while(count%3 == 0 || Integer.toString(count).contains("3")){
+                count++;
+            }
+        }
+        return count;
+    }
+}
