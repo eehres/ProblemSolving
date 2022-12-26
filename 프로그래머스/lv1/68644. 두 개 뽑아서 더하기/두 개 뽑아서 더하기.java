bc4d100adddab8efe6237f91542e88ee5b3f50ba
eehres <89247924+eehres@users.noreply.github.com>
2022-12-26 22:54:59 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 2.92 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] numbers) {
+        HashSet<Integer>hs = new HashSet<>();
+        for(int i = 0 ; i < numbers.length ; i++){
+            for(int j = i+1 ; j < numbers.length ; j++){
+                hs.add(numbers[i]+numbers[j]);
+            }
+        }
+        int[] answer =  hs.stream().mapToInt(x->x).toArray();
+        Arrays.sort(answer);
+        return answer;
+    }
+}
