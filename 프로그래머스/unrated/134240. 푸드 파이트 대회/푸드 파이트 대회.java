4ca3cd304c04d3832edfe49262e5ebb5669d485c
eehres <89247924+eehres@users.noreply.github.com>
2023-01-12 21:33:31 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 12.15 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        for(int i = 1 ; i < food.length ; i++){
+            answer+=Integer.toString(i).repeat(food[i]/2);
+        }
+                     answer+="0";
+        for(int i = answer.length()-2 ; i>=0 ; i--){
+            answer+=answer.charAt(i);
+        }
+        return answer;
+    }
+}
