6f9f15f714748ac072c57e8f3663cf3098eeff77
eehres <89247924+eehres@users.noreply.github.com>
2023-02-08 01:29:25 +0900

    [unrated] Title: 과일 장수, Time: 0.34 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -1,10 +1,10 @@
-import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
         Arrays.sort(score);
-        for(int i = score.length-m ; i >= 0 ; i-=m){
-            answer += score[i]*m;
+        int i = score.length-1;
+        while(i>0){
+            answer = score[score.length-m-1]*m;
         }
         
         return answer;
