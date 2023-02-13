14338ac2b491dc0f67675a402f1048d169904bbc
eehres <89247924+eehres@users.noreply.github.com>
2023-02-14 01:04:13 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.71 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,10 +1,10 @@
 class Solution {
     public int solution(int number, int limit, int power) {
-        int answer = 0;
+        int answer = 0; 
         for(int k = 1 ; k <= number ; k++){
-            int count = 0;
+            int count = 0;//약수의 개수
             for(int i = 1 ; i*i<=k ; i++){
-            if(i*i==k){
+            if(i*i==k){ 
                 count++;
             }else if(k%i == 0){
                 count+=2;
