57567ade7ec4d0ecc5e9080e6922fbdda31a7dea
eehres <89247924+eehres@users.noreply.github.com>
2023-02-26 21:15:30 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.03 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,24 +1,19 @@
+import java.util.*;
 class Solution {
     public int solution(int[] arr) {
-        int answer = arr[0];
-        
-        for(int i = 1;i<arr.length;i++){
-            int gcd = gcd(answer,arr[i]);
-            answer = answer * arr[i] / gcd; //최소공배수 구하기
+        int answer = 1;
+        int k = 2;
+        for(int i : arr){
+            answer*=i;
         }
-        
+        while(answer>k){
+            if(answer%k==0){
+                answer/=k;
+            }else{
+                k++;
+            }
+        }
+
         return answer;
-    }
-        public static int gcd(int a, int b){
-        int x = Math.max(a,b);
-        int y = Math.min(a,b);
-        
-        while(x % y != 0){
-            int r = x % y; //나머지
-            x = y;
-            y = r;
-        }
-        
-        return y; //유클리드 호제법으로 구한 최대공약수
     }
 }
